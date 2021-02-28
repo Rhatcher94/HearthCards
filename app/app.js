ad2dd6914ce20933336a7b53990446f25de8fa33
Rob <rhatcher94@outlook.com>
2021-02-28 12:47:33 -0500

    Deck Card limit 2
    Added an amount field to cards in the deck limiting it to 2 of a single card.
    
    When removing a card from the deck that has an amount of 2, we subtract one instead of removing it .

--- app/app.js
+++ app/app.js
@@ -1,91 +1,62 @@
-var search = document.getElementById("cardName");
-var pageNextBtn = document.getElementById("NextPage");
-var pagePrevBtn = document.getElementById("PrevPage");
-var getCardsBtn = document.getElementById("GetCards");
-var viewDeckBtn = document.getElementById("ViewDeck");
-var classList = document.getElementById("classList");
-var addCard = document.querySelectorAll("addCard");
-var className = document.querySelectorAll("dropdown-item");
+var search = document.getElementById('cardName')
+var pageNextBtn = document.getElementById('NextPage')
+var pagePrevBtn = document.getElementById('PrevPage')
+var getCardsBtn = document.getElementById('GetCards')
+var viewDeckBtn = document.getElementById('ViewDeck')
+var classList = document.getElementById('classList')
+var addCard = document.querySelectorAll('addCard')
+var className = document.querySelectorAll('dropdown-item')
 
-var pageNextBtns = document.querySelectorAll("#NextPage");
-var pagePrevBtns = document.querySelectorAll("#PrevPage");
-var pageNumbers = document.querySelectorAll("#pages");        
+var pageNextBtns = document.querySelectorAll('#NextPage')
+var pagePrevBtns = document.querySelectorAll('#PrevPage')
+var pageNumbers = document.querySelectorAll('#pages')
 
+const cardsToShow = 12
 
-const cardsToShow = 12;
+var startIndex = 0
+var endIndex = cardsToShow
 
-var startIndex = 0; 
-var endIndex = cardsToShow;
+const cards = new Array()
+const myDeck = new Array()
 
-const cards = new Array();
-const myDeck = new Array();
-
-
-
-
-const RAPIDAPI_API_URL = 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards';
-const RAPIDAPI_REQUEST_HEADERS = {    
-    "content-type":"application/octet-stream",
-    "x-rapidapi-host":"omgvamp-hearthstone-v1.p.rapidapi.com",
-    "x-rapidapi-key":"59489fd7femshd11da986701b4cep1b9b50jsn3ce72df24df4"
-  };
-
-  
-
-
-
-
-
-
-
+const RAPIDAPI_API_URL = 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards'
+const RAPIDAPI_REQUEST_HEADERS = {
+	'content-type': 'application/octet-stream',
+	'x-rapidapi-host': 'omgvamp-hearthstone-v1.p.rapidapi.com',
+	'x-rapidapi-key': '59489fd7femshd11da986701b4cep1b9b50jsn3ce72df24df4',
+}
 
 /* ------------------------ ON CLICKS --------------------------------------*/
 
-//Display call cards in the array. 
-getCardsBtn.onclick = function() {
-  startIndex = 0;
-  endIndex = cardsToShow;
-  showCards();
-};
+//Display call cards in the array.
+getCardsBtn.onclick = function () {
+	startIndex = 0
+	endIndex = cardsToShow
+	showCards()
+}
 
+pageNextBtns.forEach((btn) => {
+	btn.onclick = function () {
+		startIndex += cardsToShow
+		endIndex += cardsToShow
+		showCards()
+	}
+})
 
+pagePrevBtns.forEach((btn) => {
+	btn.onclick = function () {
+		startIndex = startIndex == 0 ? 0 : startIndex - cardsToShow
+		endIndex = endIndex == cardsToShow ? cardsToShow : endIndex - cardsToShow
+		showCards()
+	}
+})
 
-pageNextBtns.forEach(btn =>{
-  btn.onclick = function() {
-    startIndex += cardsToShow; 
-    endIndex += cardsToShow;
-    showCards()
-  };
-});
-
-
-
-pagePrevBtns.forEach(btn =>{
-  btn.onclick = function() {
-    startIndex = (startIndex == 0) ? 0 : startIndex - cardsToShow ; 
-    endIndex= (endIndex == cardsToShow) ?  cardsToShow : endIndex - cardsToShow ; 
-    showCards()
-  };
-});
-
-
-
-viewDeckBtn.onclick = function() {
-  showDeck();
-};
-
-
-
-
-
-
-
-
-
-
+viewDeckBtn.onclick = function () {
+	showDeck()
+}
 
 /* -----------------------------------------------------------------------------*/
-  loadCardAPI();
+loadCardAPI()
 
 /* 
   This API does not have a limit/Pagination PARAM available, 
@@ -93,197 +64,244 @@ viewDeckBtn.onclick = function() {
   This is slow but is simple pagination. 
 */
 
+//The initial api call is slow but there is a lack of params available with this api.
 //Called to load the Cards array in from API end point.
-function loadCardAPI(){  
-  getCardsBtn.style.display = "none"; 
-  viewDeckBtn.style.display = "none";  
-  classList.style.display = "none";
-  hideElements();
-  document.getElementById("cards").innerHTML = "Fetching Data from RapidApi"
-  axios.get(RAPIDAPI_API_URL, { headers: RAPIDAPI_REQUEST_HEADERS })
-    .then(response => { 
-      const data = response.data.Basic;    
-      data.forEach(element => {    
-        //Filters for the cards we want.
-        if((element.type != "Hero") && (element.type != "Hero Power") && !element.cardId.includes("DFX") && element.img){
-          cards.push(element);        
-        }              
-        finishLoad();
-      });           
-    })
-    .catch(error => document.getElementById("cards").innerHTML = "Unable To find Cards... " + error);  
+function loadCardAPI() {
+	getCardsBtn.style.display = 'none'
+	viewDeckBtn.style.display = 'none'
+	classList.style.display = 'none'
+	hideElements()
+	document.getElementById('cards').innerHTML = 'Fetching Data from RapidApi'
+	axios
+		.get(RAPIDAPI_API_URL, {headers: RAPIDAPI_REQUEST_HEADERS})
+		.then((response) => {
+			const data = response.data.Basic
+			data.forEach((element) => {
+				//Filters for the cards we want.
+				if (
+					element.type != 'Hero' &&
+					element.type != 'Hero Power' &&
+					!element.cardId.includes('DFX') &&
+					element.img
+				) {
+					cards.push(element)
+				}
+				finishLoad()
+			})
+		})
+		.catch(
+			(error) =>
+				(document.getElementById('cards').innerHTML = 'Unable To find Cards... ' + error)
+		)
 }
 
-//Called to display the cards. 
-function showCards(){
-  var ctr = 0;       
-  document.getElementById("cards").innerHTML = "";           
-  cards.forEach(element => {             
-      if(ctr >= startIndex && ctr < endIndex){
-        setPages();
-        document.getElementById("cards").innerHTML +=(
-          "<div class=\"col-sm-6 col-md-3\" id=\"cards\">"+    
-          "<div class=\"card sm-3 \">" +
-            "<img class=\"card-img-top\" src= '" + element.img + " 'alt=\"Failed to Load Image\" onerror=\"this.src='resources/cardPlaceHolder.png'\";> " +
-            "<div class=\"card-body\">"+
-              "<h4 class=\"card-title\"> " + element.name + "</h4>" +
-              "<p class=\"card-text\">" +   element.playerClass + ".</p>"+
-        " <button class=\"btn btn-primary\" id='addCard' data-id='"+ element.cardId + "'>" + "ADD CARD" + "</button>"+
-        "</div></div></div>");                      
-      }          
-      ctr ++;
-  });   
-  //Add onclick listener to each button.
-  addCard = document.querySelectorAll("#addCard");   
-    addCard.forEach(btn =>{
-      btn.onclick = function(){
-        addCardToDeck(btn.getAttribute("data-id"));
-      }
-    }); 
+//Called to display the cards.
+function showCards() {
+	var ctr = 0
+	document.getElementById('cards').innerHTML = ''
+	cards.forEach((element) => {
+		if (ctr >= startIndex && ctr < endIndex) {
+			setPages()
+			document.getElementById('cards').innerHTML +=
+				'<div class="col-sm-6 col-md-3" id="cards">' +
+				'<div class="card sm-3 ">' +
+				'<img class="card-img-top" src= \'' +
+				element.img +
+				' \'alt="Failed to Load Image" onerror="this.src=\'resources/cardPlaceHolder.png\'";> ' +
+				'<div class="card-body">' +
+				'<h4 class="card-title"> ' +
+				element.name +
+				'</h4>' +
+				'<p class="card-text">' +
+				element.playerClass +
+				'.</p>' +
+				" <button class=\"btn btn-primary\" id='addCard' data-id='" +
+				element.cardId +
+				"'>" +
+				'ADD CARD' +
+				'</button>' +
+				'</div></div></div>'
+		}
+		ctr++
+	})
+	//Add onclick listener to each button.
+	addCard = document.querySelectorAll('#addCard')
+	addCard.forEach((btn) => {
+		btn.onclick = function () {
+			addCardToDeck(btn.getAttribute('data-id'))
+		}
+	})
 }
 
-  function showDeck(){   
-    hideElements();
-    console.log(myDeck.length);
+function showDeck() {
+	hideElements()
+	console.log(myDeck.length)
 
-    if(myDeck.length == 0){
-      document.getElementById("cards").innerHTML = (
-      "<div class=\"col-sm-6 col-md-3\" id=\"cards\"> No Cards in Deck.  </div>");   
-    }else{
-      myDeck.forEach(element => {       
-            document.getElementById("cards").innerHTML +=(
-              "<div class=\"col-sm-6 col-md-3\" id=\"cards\">"+    
-              "<div class=\"card sm-3 \">" +
-                "<img class=\"card-img-top\" src= '" + element.img + " 'alt=\"Failed to Load Image\" onerror=\"this.src='resources/cardPlaceHolder.png'\";> " +
-                "<div class=\"card-body\">"+
-                  "<h4 class=\"card-title\"> " + element.name + "</h4>" +
-                  "<p class=\"card-text\">" +   element.playerClass + ".</p>"+
-            " <button class=\"btn btn-primary\" id='removeCard' data-id='"+ element.cardId + "'>" + "Remove Card" + "</button>"+
-            "</div></div></div>");       
-      });   
-  }
-  //Add onclick listener to each button.
-  removeCard = document.querySelectorAll("#removeCard");   
-  removeCard.forEach(btn =>{
-    btn.onclick = function(){
-      removeCardFromDeck(btn.getAttribute("data-id"));
-    }
-  }); 
+	if (myDeck.length == 0) {
+		document.getElementById('cards').innerHTML =
+			'<div class="col-sm-6 col-md-3" id="cards"> No Cards in Deck.  </div>'
+	} else {
+		myDeck.forEach((element) => {
+			document.getElementById('cards').innerHTML +=
+				'<div class="col-sm-6 col-md-3" id="cards">' +
+				'<div class="card sm-3 ">' +
+				'<img class="card-img-top" src= \'' +
+				element.img +
+				' \'alt="Failed to Load Image" onerror="this.src=\'resources/cardPlaceHolder.png\'";> ' +
+				'<div class="card-body">' +
+				'<h4 class="card-title"> ' +
+				element.name +
+				'<span class="ml-3"> x' +
+				element.amount +
+				'</span>' +
+				'</h4>' +
+				'<p class="card-text">' +
+				element.playerClass +
+				'.</p>' +
+				" <button class=\"btn btn-primary\" id='removeCard' data-id='" +
+				element.cardId +
+				"'>" +
+				'Remove Card' +
+				'</button>' +
+				'</div></div>'
+		})
+	}
+	//Add onclick listener to each button.
+	removeCard = document.querySelectorAll('#removeCard')
+	removeCard.forEach((btn) => {
+		btn.onclick = function () {
+			removeCardFromDeck(btn.getAttribute('data-id'))
+		}
+	})
 }
 
-
-
-function getACard(cardName){
- //Search the array for a card by name. 
-  
+function getACard(cardName) {
+	//Search the array for a card by name.
 }
 
+//Called when the Cards finish loading into the array.
+function finishLoad() {
+	getCardsBtn.style.display = 'block'
+	viewDeckBtn.style.display = 'block'
+	classList.style.display = 'block'
 
-//Called when the Cards finish loading into the array. 
-function finishLoad(){
-  getCardsBtn.style.display = "block";   
-  viewDeckBtn.style.display = "block"; 
-  classList.style.display = "block";
-  
-  className = document.querySelectorAll(".dropdown-item");   
-  className.forEach(btn =>{
-    btn.onclick = function(){      
-      showClassCards(btn.getAttribute("data-class"));
-    }
-  }); 
+	className = document.querySelectorAll('.dropdown-item')
+	className.forEach((btn) => {
+		btn.onclick = function () {
+			showClassCards(btn.getAttribute('data-class'))
+		}
+	})
 
-
-  document.getElementById("cards").innerHTML = ""; 
-  showCards();      
+	document.getElementById('cards').innerHTML = ''
+	showCards()
 }
 
+function setPages() {
+	var maxPages = Math.round(cards.length / cardsToShow)
+	var currPage = Math.round(endIndex / cardsToShow)
 
-function setPages(){
-  var maxPages =  Math.round(cards.length/cardsToShow); 
-  var currPage =  Math.round(endIndex/cardsToShow);
-
-  if(currPage == maxPages){
-    pageNextBtns.forEach(btn =>{
-      btn.style.display = "none";
-    });     
-    pagePrevBtns.forEach(btn =>{
-      btn.style.display = "block";
-    });
-  }else if(currPage == 1){
-    pageNextBtns.forEach(btn =>{
-      btn.style.display = "block";
-    });     
-    pagePrevBtns.forEach(btn =>{
-      btn.style.display = "none";
-    });
-  }else{
-    pageNextBtns.forEach(btn =>{
-      btn.style.display = "block";
-    });     
-    pagePrevBtns.forEach(btn =>{
-      btn.style.display = "block";
-    });
-  }
-  pageNumbers.forEach(ele =>{
-      ele.innerHTML = "Page " + currPage + " - "+ maxPages;
-  });   
+	if (currPage == maxPages) {
+		pageNextBtns.forEach((btn) => {
+			btn.style.display = 'none'
+		})
+		pagePrevBtns.forEach((btn) => {
+			btn.style.display = 'block'
+		})
+	} else if (currPage == 1) {
+		pageNextBtns.forEach((btn) => {
+			btn.style.display = 'block'
+		})
+		pagePrevBtns.forEach((btn) => {
+			btn.style.display = 'none'
+		})
+	} else {
+		pageNextBtns.forEach((btn) => {
+			btn.style.display = 'block'
+		})
+		pagePrevBtns.forEach((btn) => {
+			btn.style.display = 'block'
+		})
+	}
+	pageNumbers.forEach((ele) => {
+		ele.innerHTML = 'Page ' + currPage + ' - ' + maxPages
+	})
 }
 
-function addCardToDeck(newCardId){
-  for(let element of cards){
-    if(element.cardId == newCardId){
-      myDeck.push(element);
-      break;
-    }
-  }
+function addCardToDeck(newCardId) {
+	for (let element of cards) {
+		if (element.cardId == newCardId) {
+			const exists = myDeck.find((card) => card.cardId === element.cardId)
+			if (!exists) {
+				element.amount = 1
+				myDeck.push(element)
+			} else if (element.amount != 2) {
+				exists.amount = 2
+			}
+			console.log(myDeck)
+
+			break
+		}
+	}
 }
 
+function removeCardFromDeck(oldCardId) {
+	const card = myDeck.find((card) => card.cardId === oldCardId)
+	if (card.amount === 2) {
+		card.amount = 1
+	} else {
+		for (var index = 0; index < myDeck.length; index++) {
+			if (myDeck[index].cardId == oldCardId) {
+				myDeck.splice(index, 1)
+				break
+			}
+		}
+	}
 
-function removeCardFromDeck(oldCardId){
-  for(var index = 0; index < myDeck.length; index++){
-    if(myDeck[index].cardId == oldCardId){
-      myDeck.splice(index, 1);      
-      break;
-    }
-  }
-  showDeck(); 
+	showDeck()
 }
 
-
-function showClassCards(className){
-   
-hideElements();           
-  cards.forEach(element => { 
-        if(element.playerClass == className){
-          document.getElementById("cards").innerHTML +=(
-            "<div class=\"col-sm-6 col-md-3\" id=\"cards\">"+    
-            "<div class=\"card sm-3 \">" +
-              "<img class=\"card-img-top\" src= '" + element.img + " 'alt=\"Failed to Load Image\" onerror=\"this.src='resources/cardPlaceHolder.png'\";> " +
-              "<div class=\"card-body\">"+
-                "<h4 class=\"card-title\"> " + element.name + "</h4>" +
-                "<p class=\"card-text\">" +   element.playerClass + ".</p>"+
-          " <button class=\"btn btn-primary\" id='addCard' data-id='"+ element.cardId + "'>" + "ADD CARD" + "</button>"+
-          "</div></div></div>");                      
-           
-      }
-  });   
-  //Add onclick listener to each button.
-  addCard = document.querySelectorAll("#addCard");   
-    addCard.forEach(btn =>{
-      btn.onclick = function(){
-        addCardToDeck(btn.getAttribute("data-id"));
-      }
-    }); 
+function showClassCards(className) {
+	hideElements()
+	cards.forEach((element) => {
+		if (element.playerClass == className) {
+			document.getElementById('cards').innerHTML +=
+				'<div class="col-sm-6 col-md-3" id="cards">' +
+				'<div class="card sm-3 ">' +
+				'<img class="card-img-top" src= \'' +
+				element.img +
+				' \'alt="Failed to Load Image" onerror="this.src=\'resources/cardPlaceHolder.png\'";> ' +
+				'<div class="card-body">' +
+				'<h4 class="card-title"> ' +
+				element.name +
+				'</h4>' +
+				'<p class="card-text">' +
+				element.playerClass +
+				'.</p>' +
+				" <button class=\"btn btn-primary\" id='addCard' data-id='" +
+				element.cardId +
+				"'>" +
+				'ADD CARD' +
+				'</button>' +
+				'</div></div></div>'
+		}
+	})
+	//Add onclick listener to each button.
+	addCard = document.querySelectorAll('#addCard')
+	addCard.forEach((btn) => {
+		btn.onclick = function () {
+			addCardToDeck(btn.getAttribute('data-id'))
+		}
+	})
 }
 
-function hideElements(){
-  pageNextBtns.forEach(btn =>{
-    btn.style.display = "none";
-  });     
-  pagePrevBtns.forEach(btn =>{
-    btn.style.display = "none";
-  });
+function hideElements() {
+	pageNextBtns.forEach((btn) => {
+		btn.style.display = 'none'
+	})
+	pagePrevBtns.forEach((btn) => {
+		btn.style.display = 'none'
+	})
 
-  document.getElementById("pages").innerHTML = "";   
-  document.getElementById("cards").innerHTML = "";    
+	document.getElementById('pages').innerHTML = ''
+	document.getElementById('cards').innerHTML = ''
 }
